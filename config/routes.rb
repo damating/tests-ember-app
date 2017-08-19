75ed34afd929ebccd65860163d55bb7dbd3eed6b
damating <paula.grzesiak@gmail.com>
2017-08-19 16:58:16 +0200

    Rails models, controllers, db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :question_options
+  resources :questions
+  resources :tests
+  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount_ember_app :frontend, to: "/"
 end
